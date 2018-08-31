3b8ae0bd80873355c1cc1644b11d547ca4216a2c
gwenf <gwenfaraday@gmail.com>
2018-08-31 01:44:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# kitkat-coin
+
+*This project was created using this starter kit with lotion.js and shea.js: https://github.com/keppel/lotion-shea-starter*
+
+## Run the app
+
+1. coming soon...
---
+++ app.js
@@ -0,0 +1,27 @@
+let lotion = require('lotion')
+let shea = require('shea')
+
+let app = lotion({
+  initialState: {
+    count: 0
+  },
+  devMode: process.env.PRODUCTION !== 'true'
+})
+
+app.use(function (state, tx, chainInfo) {
+  // handle transactions here
+  state.count++
+})
+
+app.useBlock(function (state, chainInfo) {
+  // do something once per block here
+})
+
+app.use(shea('public/'))
+
+let port = process.env.PORT || 3000
+console.log('app listening on port: ', port)
+app.listen(port).then(appInfo => {
+  console.log(appInfo.GCI)
+  // 'f6d671670ce307f71164c7e9b7c1d89c0cf5a6456ddf0a538d59bdbd33216ec5'
+})
